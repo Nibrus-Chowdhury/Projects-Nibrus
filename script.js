e8ee9fb48b25ba3d7e93e377543014f5ae1501e0
Nibrus Chowdhury <147645175+Nibrus-Chowdhury@users.noreply.github.com>
2024-03-12 01:10:55 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function myFunction(){
+document.getElementById("greet").style.color="blue";
+}
+function night(){
+  document.body.style.backgroundColor="lightblue";
+}
+function paragraph(){
+  document.getElementById("paragraph").style.color="white";
+}
